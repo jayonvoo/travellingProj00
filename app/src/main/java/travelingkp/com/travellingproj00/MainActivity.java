e299f8e960fcfa9a93848628790ab75dbee8f124
Johnv <kiyoto_zy@hotmail.com>
2018-03-14 10:41:12 +0800

    update file

--- app/src/main/java/travelingkp/com/travellingproj00/MainActivity.java
+++ app/src/main/java/travelingkp/com/travellingproj00/MainActivity.java
@@ -3,11 +3,20 @@ package travelingkp.com.travellingproj00;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static java.lang.System.out;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        out.println("test start");
     }
 }
