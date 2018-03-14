7fbd4408275a84170f077bdcf04586ac2059f8b0
Johnv <kiyoto_zy@hotmail.com>
2018-03-14 17:58:41 +0800

    update file

--- app/src/main/java/travelingkp/com/travellingproj00/MainActivity.java
+++ app/src/main/java/travelingkp/com/travellingproj00/MainActivity.java
@@ -2,22 +2,39 @@ package travelingkp.com.travellingproj00;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import static java.lang.System.out;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    TextView textView;
+    EditText editText;
+    String getText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button2);
+        textView = (TextView) findViewById(R.id.textView3);
+        editText = (EditText) findViewById(R.id.editText6);
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getText = editText.getText().toString();
+                textView.setText(getText);
+            }
+        });
     }
-
-    @Override
-        protected void onStart() {
-        super.onStart();
-
-        //test06
-    }
-
 }
+
+
