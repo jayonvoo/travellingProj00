29f77332a9cf06578ddbc578d962dc1789c5c7a6
Johnv <kiyoto_zy@hotmail.com>
2018-03-14 10:21:02 +0800

    update

---
+++ app/src/main/java/travelingkp/com/travellingproj00/MainActivity.java
@@ -0,0 +1,13 @@
+package travelingkp.com.travellingproj00;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
